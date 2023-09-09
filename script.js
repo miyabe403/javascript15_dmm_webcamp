df4b8df330d58d7bb2d889df8833d0e60fcbc2e5
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-10 08:06:50 +0900

    [Start] Turbolinksを理解しよう

---
+++ script.js
@@ -0,0 +1,9 @@
+// 変更前
+// $(document).ready(function () {
+//   alert('Hello World')
+// });
+
+$(document).on('turbolinks:load', function () {
+  alert('Hello World')
+});
+                                 
