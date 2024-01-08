8adc97fd7e31469c1fe97282fedf6bf960589da4
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2024-01-09 02:49:44 +0900

    [Start] 1/9 学習開始

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,6 +1,6 @@
 // 変更前
 // $(document).ready(function () {
 //   alert('Hello World')
-// });v
+// });
 $(document).on('turbolinks:load', function ()  
-});                                                          　    
+});                                                          　     　
