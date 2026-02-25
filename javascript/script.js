75422d11d6fbda5e613b793ec0a32a1f070e44a5
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2026-02-26 06:48:31 +0900

    [Finish] 2/25 学習終了

--- javascript/script.js
+++
@@ -1,9 +0,0 @@
-// 変更前
-// $(document).ready(function () {
-//   alert('Hello World')
-// });
-
-$(document).on('turbolinks:load', function () {
-  alert('Hello World')
-});
-                                 
